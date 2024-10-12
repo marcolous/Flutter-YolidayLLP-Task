29658527edc49d9267fa961c0366f6c1b77502e7
marco <119810706+array-marco@users.noreply.github.com>
2024-10-12 19:09:51 +0300

    project list view

---
+++ lib/feature/protfolio/presentation/view/screens/project widgets/project_list_view.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:yoliday_llp_task/core/utils/no_bounce_scroll_behavior.dart';
+import 'package:yoliday_llp_task/feature/protfolio/presentation/view/screens/project%20widgets/project_item.dart';
+
+class ProjectListView extends StatelessWidget {
+  const ProjectListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScrollConfiguration(
+      behavior: NoBounceScrollBehavior(),
+      child: Expanded(
+        child: ListView.separated(
+          itemBuilder: (context, index) => ProjectItem(),
+          separatorBuilder: (context, index) => Gap(10),
+          itemCount: 20,
+        ),
+      ),
+    );
+  }
+}
--- lib/feature/protfolio/presentation/view/screens/project.dart
+++ lib/feature/protfolio/presentation/view/screens/project.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:gap/gap.dart';
-import 'package:yoliday_llp_task/core/styles/styles.dart';
-import 'package:yoliday_llp_task/core/utils/app_images.dart';
 import 'package:yoliday_llp_task/core/widgets/search_widget.dart';
+import 'package:yoliday_llp_task/feature/protfolio/presentation/view/screens/project%20widgets/project_list_view.dart';
 
 class Project extends StatelessWidget {
   const Project({super.key});
@@ -12,106 +10,8 @@ class Project extends StatelessWidget {
     return const Column(
       children: [
         SearchWidget(),
-        ProjectItem(),
+        ProjectListView(),
       ],
-    );
-  }
-}
-
-class ProjectItem extends StatelessWidget {
-  const ProjectItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 16),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(12),
-        border: Border.all(color: Color(0xffE0E0E0)),
-      ),
-      child: SizedBox(
-        height: 120,
-        child: Row(
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(12),
-                topLeft: Radius.circular(12),
-              ),
-              child: AppImages.image,
-            ),
-            Expanded(
-              child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Expanded(
-                      child: Text(
-                        'Kemampuan Merangkum Tulisan',
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
-                        style: Styles.style15Medium(context)
-                            .copyWith(color: Colors.black),
-                      ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'BAHASA SUNDA',
-                              style: Styles.style12Med(context),
-                            ),
-                            Text(
-                              'Oleh Al-Baiqi Samaan',
-                              style: Styles.style12Med(context)
-                                  .copyWith(color: Color(0xff9E95A2)),
-                            ),
-                          ],
-                        ),
-                        Container(
-                          width: 70,
-                          height: 40,
-                          decoration: BoxDecoration(
-                            gradient: const LinearGradient(
-                              colors: [Color(0xffF39519), Color(0xffFFCD67)],
-                              begin: Alignment.topCenter,
-                              end: Alignment.bottomCenter,
-                            ),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: ElevatedButton(
-                            onPressed: () {},
-                            style: ElevatedButton.styleFrom(
-                              padding: EdgeInsets.zero,
-                              backgroundColor: Colors.transparent,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(20),
-                              ),
-                              elevation: 0,
-                            ),
-                            child: Text(
-                              'A',
-                              style: Styles.style18SemiBold(context)
-                                  .copyWith(color: Colors.white),
-                            ),
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-              ),
-            )
-          ],
-        ),
-      ),
     );
   }
 }
